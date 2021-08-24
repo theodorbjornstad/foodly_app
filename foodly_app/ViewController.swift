a423d1ecf7c999b5c2517c3d6ae7fe84e6679993
Theodor Holmen Bjørnstad <theodor.bjornstad@bouvet.no>
2021-08-24 10:25:00 +0200

    Initial Commit

---
+++ foodly_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodly_app
+//
+//  Created by Theodor Holmen Bjørnstad on 24/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
